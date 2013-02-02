924c0aae187ef9316df67de7ac1e443a8e34211a
Stanislav Ushakov <stasushakov@gmail.com>
2013-02-02 21:02:26 +0400

    Added ImmuneSystem class and auxiliary class (FitnessFunction and  ExpressionMutator).

---
+++ immune.py
@@ -0,0 +1,223 @@
+__author__ = 'Stanislav Ushakov'
+
+import math
+import random
+import copy
+
+from expression import Expression
+
+class FitnessFunction:
+    """
+    Used for calculating fitness function for
+    given expression.
+    Value is simple Euclidean norm for vector.
+    """
+    def __init__(self, exact_values):
+        """
+        Initializes function with the exact values of the needed function.
+        Pass exact values in the following form:
+        [({'x': 1, 'y': 1}, 0.125),
+         ({'x': 2, 'y': 2}, 0.250)]
+        """
+        self.exact_values = exact_values
+
+    def expression_value(self, expression:Expression):
+        """
+        Returns value of the fitness function for given
+        expression. The less the value - the closer expression to
+        the unknown function.
+        """
+        sum = 0
+        for (variables, value) in self.exact_values:
+            sum += ((expression.value_in_point(variables) - value) *
+                    (expression.value_in_point(variables) - value))
+        return math.sqrt(sum)
+
+
+class ExpressionMutator:
+    """
+    This class encapsulates all logic for mutating selected lymphocytes.
+    """
+    def __init__(self, expression:Expression):
+        """
+        Initializes mutator with the given expression.
+        NOTE: expression itself won't be changed. Instead of its
+        changing, the new expression will be returned.
+        """
+        self.expression = copy.deepcopy(expression)
+        self.mutations = [
+            self.number_mutation,
+            self.variable_mutation,
+            self.unary_mutation,
+            self.binary_mutation,
+            self.subtree_mutation]
+
+    def mutation(self):
+        """
+        Returns the mutated version of the expression.
+        All mutations are of equal possibilities.
+        May be change.
+        """
+        mutation = random.choice(self.mutations)
+        mutation()
+        return self.expression
+
+    def number_mutation(self):
+        """
+        USed for mutate number nodes. Adds or subtracts random number from
+        the value or
+        """
+        numbers = self._get_all_nodes_by_filter(lambda n: n.is_number())
+        if not numbers: return
+
+        selected_node = random.choice(numbers)
+        if random.random() < 0.45:
+            selected_node.value += random.random()
+        elif random.random() < 0.9:
+            selected_node.value -= random.random()
+        else:
+            selected_node.value = round(selected_node.value)
+
+    def variable_mutation(self):
+        """
+        Changes one randomly selected variable to another, also
+        randomly selected.
+        """
+        variables = self._get_all_nodes_by_filter(lambda n: n.is_variable())
+        if not variables: return
+
+        selected_var = random.choice(variables)
+        selected_var.value = random.choice(self.expression.variables)
+
+    def unary_mutation(self):
+        """
+        Changes one unary operation to another
+        """
+        unary_operations = self._get_all_nodes_by_filter(lambda n: n.is_unary())
+        if not unary_operations: return
+
+        selected_unary = random.choice(unary_operations)
+        selected_unary.operation = random.choice(self.expression.get_unary_operations())
+
+    def binary_mutation(self):
+        """
+        Changes one binary operations to another
+        """
+        binary_operations = self._get_all_nodes_by_filter(lambda n: n.is_binary())
+        if not binary_operations: return
+
+        selected_binary = random.choice(binary_operations)
+        selected_binary.operation = random.choice(self.expression.get_binary_operations())
+
+    def subtree_mutation(self):
+        """
+        Changes one randomly selected node to the randomly generated subtree.
+        The height of the tree isn't changed.
+        """
+        nodes = self._get_all_nodes_by_filter(lambda n: n.height() > 1 and
+                                                        n != self.expression.root)
+        if not nodes: return
+
+        selected_node = random.choice(nodes)
+        max_height = self.expression.root.height() - selected_node.height()
+        new_subtree = Expression.generate_random(max_height, self.expression.variables)
+        selected_node.operation = new_subtree.root.operation
+        selected_node.value = new_subtree.root.value
+        selected_node.left = new_subtree.root.left
+        selected_node.right = new_subtree.root.right
+
+    def _get_all_nodes_by_filter(self, filter_func):
+        """
+        Used for selecting all nodes satisfying the given filter.
+        """
+        nodes = []
+
+        def traverse_tree(node):
+            if filter_func(node):
+                nodes.append(node)
+            if node.left is not None:
+                traverse_tree(node.left)
+            if node.right is not None:
+                traverse_tree(node.right)
+        traverse_tree(self.expression.root)
+
+        return nodes
+
+
+class ExpressionsImmuneSystem:
+    """
+    Class represents entire immune system.
+    Now - this is simply algorithm, that works for a number of steps.
+    On each step the best lymphocytes are selected for the mutation.
+    """
+    maximal_height = 4
+    number_of_lymphocytes = 100
+    number_of_iterations = 30
+
+    def __init__(self, exact_values, variables, **configuration):
+        """
+        Initializes the immune system with the provided parameters (or
+        default parameters if not provided).
+        lymphocytes - list that stores current value of the whole system.
+        """
+        self.exact_values = exact_values
+        self.variables = variables
+        self.fitness_function = FitnessFunction(exact_values)
+
+        #config
+        self.number_of_lymphocytes = (
+            configuration['number_of_lymphocytes']
+            if 'number_of_lymphocytes' in configuration
+            else ExpressionsImmuneSystem.number_of_lymphocytes)
+        self.number_of_iterations = (
+            configuration['number_of_iterations']
+            if 'number_of_iterations' in configuration
+            else ExpressionsImmuneSystem.number_of_iterations)
+
+        self.lymphocytes = []
+        for i in range(0, self.number_of_lymphocytes):
+            self.lymphocytes.append(Expression.generate_random(
+                                        ExpressionsImmuneSystem.maximal_height,
+                                        variables))
+
+        random.seed()
+
+    def solve(self):
+        """
+        After defined number of steps returns the best lymphocyte as
+        an answer.
+        """
+        for i in range(0, self.number_of_lymphocytes):
+            self.step()
+        self.best().simplify()
+
+        return self.best()
+
+    def step(self):
+        """
+        Represents the step of the solution finding.
+        The half of the lymphocytes are mutated. The new system
+        consists of this half and their mutated 'children'.
+        """
+        sorted_lymphocytes = self._get_sorted_lymphocytes_index_and_value()
+        best = []
+        for (i, e) in sorted_lymphocytes[:self.number_of_lymphocytes // 2]:
+            best.append(self.lymphocytes[i])
+        mutated = [ExpressionMutator(e).mutation() for e in best]
+        self.lymphocytes = best + mutated
+
+    def best(self):
+        """
+        Returns the best lymphocyte in the system.
+        """
+        return self.lymphocytes[self._get_sorted_lymphocytes_index_and_value()[0][0]]
+
+    def _get_sorted_lymphocytes_index_and_value(self):
+        """
+        Returns list of lymphocytes and their numbers in the original system
+        in sorted order.
+        """
+        fitness_values = []
+        for (i, e) in enumerate(self.lymphocytes):
+            fitness_values.append((i, self.fitness_function.expression_value(e)))
+        return sorted(fitness_values, key=lambda item: item[1])
--- tests.py
+++ tests.py
@@ -3,6 +3,7 @@ __author__ = 'Stanislav Ushakov'
 import unittest
 
 from expression import Expression, NotSupportedOperationError
+from immune import FitnessFunction, ExpressionMutator, ExpressionsImmuneSystem
 
 class ExpressionNodeTest(unittest.TestCase):
     def test_allowed_operation(self):
@@ -16,17 +17,85 @@ class ExpressionNodeTest(unittest.TestCase):
 
     def test_value_in_point(self):
         node = Expression.Node('MINUS',
-                left=Expression.Node('IDENTITY',left=None, right=None, value='x'),
-                right=Expression.Node('IDENTITY',left=None, right=None, value='y'))
+                left=Expression.Node('IDENTITY', value='x'),
+                right=Expression.Node('IDENTITY', value='y'))
         result = node.value_in_point({'x': 2, 'y': 1})
         self.assertEqual(result, 1.0)
 
     def test_simplify(self):
         node = Expression.Node('MINUS',
-            left=Expression.Node('NUMBER',left=None, right=None, value=2),
-            right=Expression.Node('NUMBER',left=None, right=None, value=2))
+            left=Expression.Node('NUMBER', value=2),
+            right=Expression.Node('NUMBER', value=2))
         result = node.simplify()
         self.assertEqual(result, True)
         self.assertEqual(node.left, None)
         self.assertEqual(node.right, None)
         self.assertEqual(node.value, 0.0)
+
+
+class FitnessFunctionTest(unittest.TestCase):
+    def setUp(self):
+        values = [({'x': i , 'y': j}, 4 * i + 2 * j)
+                  for i in range(0, 10)
+                  for j in range(0, 10)]
+        self.f = FitnessFunction(values)
+
+    def test_exact_value(self):
+        answer = Expression.Node('PLUS',
+            Expression.Node('MULTIPLICATION',
+                left=Expression.Node('IDENTITY', value='x'),
+                right=Expression.Node('NUMBER', value=4)),
+            Expression.Node('MULTIPLICATION',
+                left=Expression.Node('IDENTITY', value='y'),
+                right=Expression.Node('NUMBER', value=2)))
+        e = Expression(root=answer, variables=['x', 'y'])
+        self.assertEqual(self.f.expression_value(e), 0.0)
+
+    def test_wrong_value(self):
+        wrong = Expression.Node('MINUS',
+            Expression.Node('MULTIPLICATION',
+                left=Expression.Node('IDENTITY', value='x'),
+                right=Expression.Node('NUMBER', value=4)),
+            Expression.Node('MULTIPLICATION',
+                left=Expression.Node('IDENTITY', value='y'),
+                right=Expression.Node('NUMBER', value=2)))
+        e = Expression(root=wrong, variables=['x', 'y'])
+        self.assertGreater(self.f.expression_value(e), 0.0)
+
+
+class ExpressionMutatorTest(unittest.TestCase):
+    def setUp(self):
+        root = Expression.Node('PLUS',
+            Expression.Node('MULTIPLICATION',
+                left=Expression.Node('IDENTITY', value='x'),
+                right=Expression.Node('NUMBER', value=4)),
+            Expression.Node('MULTIPLICATION',
+                left=Expression.Node('IDENTITY', value='y'),
+                right=Expression.Node('NUMBER', value=2)))
+        self.f = Expression(root=root, variables=['x', 'y'])
+
+    def test_number_mutation(self):
+        mutator = ExpressionMutator(expression=self.f)
+        mutator.number_mutation()
+        point = {'x': 1, 'y': 2}
+        original_value = self.f.value_in_point(point)
+        mutated_value = mutator.expression.value_in_point(point)
+        self.assertNotEqual(original_value, mutated_value)
+
+class ExpressionsImmuneSystemTest(unittest.TestCase):
+    def setUp(self):
+        self.values = [({'x': i , 'y': j}, 4 * i + 2 * j)
+                  for i in range(0, 20)
+                  for j in range(0, 20)]
+
+    def test_that_best_is_returned(self):
+        immuneSystem = ExpressionsImmuneSystem(exact_values=self.values,
+                                               variables=['x', 'y'],
+                                               number_of_lymphocytes=10,
+                                               number_of_iterations=2)
+        f = FitnessFunction(exact_values=self.values)
+        best = immuneSystem.solve()
+        first = immuneSystem.lymphocytes[0]
+        second = immuneSystem.lymphocytes[1]
+        self.assertLessEqual(f.expression_value(best), f.expression_value(first))
+        self.assertLessEqual(f.expression_value(best), f.expression_value(second))
