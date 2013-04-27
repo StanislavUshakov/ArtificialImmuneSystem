1a9ec6dae70c1f53c12760e0d9fb6d40336202ba
Stanislav Ushakov <stasushakov@gmail.com>
2013-04-27 20:13:18 +0400

    Changed node main script according to new config object.

--- node_main.py
+++ node_main.py
@@ -1,18 +1,17 @@
 __author__ = 'Stanislav Ushakov'
 
-from immune import ExpressionsImmuneSystem, DataFileStorageHelper
+from immune import ExpressionsImmuneSystem, DataFileStorageHelper, ExpressionsImmuneSystemConfig
 from exchanger import PeerToPeerExchanger, LocalhostNodesManager
-import random
-import math
 import sys
 
 #start as "python node_main.py node_num number_of_nodes"
 if __name__ == '__main__':
     number = int(sys.argv[1])
     number_of_nodes = int(sys.argv[2])
+
     nodes_manager = LocalhostNodesManager(number, number_of_nodes)
-    number_of_lymphocytes = 150
-    max_height = 4
+    config = ExpressionsImmuneSystemConfig()
+    config.number_of_lymphocytes = 150
 
     variables, values = DataFileStorageHelper.load_from_file('test_x_y.txt')
 
@@ -22,8 +21,6 @@ if __name__ == '__main__':
     immuneSystem = ExpressionsImmuneSystem(exact_values=values,
             variables=variables,
             exchanger=exchanger,
-            number_of_lymphocytes=number_of_lymphocytes,
-            maximal_height=max_height,
-            number_of_iterations=150)
+            config=config)
     best = immuneSystem.solve()
     print(best)
